80d5be2677882e393c9c8a1434da7e1afcc7bbbf
LeshaSerov <serov1lesha@gmail.com>
2022-04-09 13:16:22 +0400

    4 Commit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,12 @@
+import polish.PolishConvertor;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String... args) {
-
+        PolishConvertor polishConvertor = new PolishConvertor();
+        String str = "1+2*3";
+        System.out.println(polishConvertor.convert(str).stream().map(Objects::toString).map(x->x+' ').collect(Collectors.joining()));
     }
 }
--- src/main/java/polish/PolishConvertor.java
+++ src/main/java/polish/PolishConvertor.java
@@ -1,13 +1,14 @@
 package polish;
 
 import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class PolishConvertor {
 
     private List<String> result = new ArrayList<>();
-    private Stack<Character> stack = new Stack();
+    private Stack<Character> stack = new Stack<>();
     private Set<Character> operands = new HashSet<>(Arrays.asList('+','-','/','*','^'));
     private List<Character> number = new ArrayList<>();
 
@@ -37,8 +38,11 @@ public class PolishConvertor {
                 processOperation(c, stack, result);
             }
         }
-        result.add(stack.stream().map(x->new StringBuilder(x).reverse()).map(String::valueOf).collect(Collectors.joining()));
-        return Collections.emptyList();
+        collectNumberAndPushToResult(number,result);
+        //.map(x->' '+x)
+        //.map(x -> new StringBuilder(x).reverse())
+        result.add(stack.stream().map(Objects::toString).map(x->x+" ").);
+        return result;
     }
 
     //region Check Type
@@ -57,11 +61,25 @@ public class PolishConvertor {
     };
     //endregion
 
+    Integer priorityOperation(char symbol)
+    {
+        if (symbol == '^')
+            return 3;
+        if (symbol == '*' || symbol == '/')
+            return 2;
+        else
+            return 1;
+    }
+
     void processDigit(char symbol, List<Character> number) {
         number.add(symbol);
     };
     void processBracket(char symbol, Stack<Character> stack, List<String> result) {};
     void processOperation(char symbol, Stack<Character> stack, List<String> result) {
+        if (!stack.empty() && (priorityOperation(stack.peek()) >= priorityOperation(symbol)))
+        {
+            result.add(String.valueOf(stack.pop()));
+        }
         stack.add(symbol);
     };
 
